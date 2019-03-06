3ebb01c09ad25cd4046ca0968d9a44221f22b65a
Alison Reilly <reillyamr@gmail.com>
2019-03-06 18:14:10 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,8 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.integer :price 
       t.string :image_url 
       t.string :size 
-      t.datetime :created
-      t.datetime :updated
+      t.timestamps :created
+      t.timestamps :updated
       
     end 
   end
